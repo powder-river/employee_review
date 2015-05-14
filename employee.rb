1bca59e7bc4bac3aba1a3764779309e33da03499
Danai Adkisson <dadkisso@live.com>
2015-05-14 13:44:23 -0400

    added test to confrim employee class knows name, email, salary, and phone number

--- employee.rb
+++ employee.rb
@@ -1,6 +1,14 @@
 class Employee
+  attr_reader :name, :email, :phone_number, :salary
 
-  
+  def initialize (name, email, phone_number, salary)
+    @name = name
+    @email = email
+    @phone_number = phone_number
+    @salary = salary
+
+  end
+
 
 
 
